18a63f36443d7221a40b9b33b1f6477b6513647c
anashaq20 <61192676+anashaq20@users.noreply.github.com>
2020-04-07 23:55:04 +0700

    Delete koneksi.inc.php

--- koneksi.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-		$servername = 'localhost';
-		$username = 'root';
-		$password = '';
-		$database = '18082010028';
-		$conn = mysqli_connect($servername,$username,$password,$database);
-		mysqli_connect($servername,$username,$password,$database) or die("Koneksi gagal dibuat");
-?>
